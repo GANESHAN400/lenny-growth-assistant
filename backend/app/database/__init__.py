f88a85aaa3d468531effeef1c4072f6c3f1cfa22
Ganeshan <ganeshancodes@gmail.com>
2026-08-01 11:48:20 +0530

    Add abstract ORM base model

--- backend/app/database/__init__.py
+++ backend/app/database/__init__.py
@@ -3,10 +3,12 @@ from app.database.dependencies import get_db
 from app.database.engine import engine
 from app.database.health import check_database_connection
 from app.database.mixins import TimestampMixin, UUIDMixin
+from app.database.model import BaseModel
 from app.database.session import SessionLocal
 
 __all__ = [
     "Base",
+    "BaseModel",
     "SessionLocal",
     "TimestampMixin",
     "UUIDMixin",
---
+++ backend/app/database/model.py
@@ -0,0 +1,6 @@
+from app.database.base import Base
+from app.database.mixins import TimestampMixin, UUIDMixin
+
+
+class BaseModel(Base, UUIDMixin, TimestampMixin):
+    __abstract__ = True
