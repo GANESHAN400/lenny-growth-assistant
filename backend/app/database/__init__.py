9528bcf5218d723a5a70f870527a5504a3c3abb9
Ganeshan <ganeshancodes@gmail.com>
2026-08-01 11:40:12 +0530

    Add database session dependency

--- backend/app/database/__init__.py
+++ backend/app/database/__init__.py
@@ -1,5 +1,6 @@
 from app.database.base import Base
+from app.database.dependencies import get_db
 from app.database.engine import engine
 from app.database.session import SessionLocal
 
-__all__ = ["Base", "SessionLocal", "engine"]
+__all__ = ["Base", "SessionLocal", "engine", "get_db"]
