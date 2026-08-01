c62df5766d9bf6cb8bbfa1c59e2c860f0943fbc3
Ganeshan <ganeshancodes@gmail.com>
2026-08-01 11:44:28 +0530

    Add timestamp mixin

--- backend/app/database/__init__.py
+++ backend/app/database/__init__.py
@@ -2,11 +2,13 @@ from app.database.base import Base
 from app.database.dependencies import get_db
 from app.database.engine import engine
 from app.database.health import check_database_connection
+from app.database.mixins import TimestampMixin
 from app.database.session import SessionLocal
 
 __all__ = [
     "Base",
     "SessionLocal",
+    "TimestampMixin",
     "check_database_connection",
     "engine",
     "get_db",
